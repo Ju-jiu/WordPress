346826d253210d8d22cb12798cc1daa2090e6ea8
Jujiu <q2335185714@qq.com>
2017-12-23 00:42:24 +0800

    暂时去掉相关社交网站链接

--- wp-content/themes/twentyseventeen/footer.php
+++ wp-content/themes/twentyseventeen/footer.php
@@ -24,14 +24,14 @@
 				if ( has_nav_menu( 'social' ) ) : ?>
 					<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
 						<?php
-							wp_nav_menu( array(
+/*							wp_nav_menu( array(
 								'theme_location' => 'social',
 								'menu_class'     => 'social-links-menu',
 								'depth'          => 1,
 								'link_before'    => '<span class="screen-reader-text">',
 								'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
 							) );
-						?>
+						*/?>    <!--暂时去掉相关社交网站链接-->
 					</nav><!-- .social-navigation -->
 				<?php endif;
 
