2347c55731b4aa400bf9214c5007a8d642c4064f
Jujiu <q2335185714@qq.com>
2017-12-23 00:53:14 +0800

    调整底部版权信息样式

--- wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
+++ wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -9,7 +9,7 @@
  */
 
 ?>
-<div class="site-info">
+<div class="site-info" style="text-align: center;">
 	<!--<a href="<?php /*echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); */?>"><?php /*printf( __( 'Copyright © %s', 'twentyseventeen' ), 'Jujiu' ); */?></a>-->
     <?php printf( __( 'Copyright © %s', 'twentyseventeen' ), 'Jujiu' ); ?>
 </div><!-- .site-info -->
